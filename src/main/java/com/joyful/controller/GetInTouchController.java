a4d865d930fd399455d67ac980438a535833be4f
SonaliRam <sonali.files654@gmail.com>
2025-07-14 18:23:56 +0530

    delete mapping added

--- src/main/java/com/joyful/controller/GetInTouchController.java
+++ src/main/java/com/joyful/controller/GetInTouchController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +38,11 @@ public class GetInTouchController {
 		return ResponseEntity.ok("Contact form submitted successfully.");
 	}
 	
+	@DeleteMapping("/delete-query/{id}")
+	public ResponseEntity<String> deleteQuery(@PathVariable Long id) {
+	    getInTouchrepo.deleteById(id);
+	    return ResponseEntity.ok("Deleted successfully.");
+	}
+
 }   
 
